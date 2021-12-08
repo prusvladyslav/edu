ab29eebc37208c5bd7795fa347c5b26fb646fc47
Prus Vladyslav <vlpr@opinov8.com>
2021-12-08 17:37:39 +0200

    1 & 2 prettier

--- src/Advanced/2-Code-Splitting/app.jsx
+++ src/Advanced/2-Code-Splitting/app.jsx
@@ -1,16 +1,15 @@
 // import { calc } from './calc'
-import React, { useState,lazy } from 'react'
+import React, { useState, lazy } from "react";
 const calc = lazy(() => import("./calc.jsx"));
 
 export const App = () => {
-    
-    const firstName = 'Vlad'
-    const lastName = 'Prus'
-    const [fullName, setFullName] = useState('')
-    setFullName(calc(firstName,lastName))
-    return (
-      <>
-        <h1>Hi,{fullName}</h1>
-      </>
-    );
-}
+  const firstName = "Vlad";
+  const lastName = "Prus";
+  const [fullName, setFullName] = useState("");
+  setFullName(calc(firstName, lastName));
+  return (
+    <>
+      <h1>Hi,{fullName}</h1>
+    </>
+  );
+};
