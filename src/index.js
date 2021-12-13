9c181e0a2825d6b2462689a2586d9920eb223ff9
Prus Vladyslav <vlpr@opinov8.com>
2021-12-13 16:52:04 +0200

    123231321

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import reportWebVitals from './reportWebVitals';
-import {Context} from './Advanced/3-Context/Context'
+import {table} from './JsBookPractise/index'
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 ReactDOM.render(
   <>
-<Context/>
+{table(5,15)}
   </>,
   document.getElementById("root")
 );
