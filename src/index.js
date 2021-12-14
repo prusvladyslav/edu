d7827cd90aa6541be4ed97ff6b72ea364c032b83
Prus Vladyslav <vlpr@opinov8.com>
2021-12-14 17:33:52 +0200

    4-ErrorBoundary

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import reportWebVitals from './reportWebVitals';
-import {table} from './JsBookPractise/index'
+import App from './Advanced/4-ErrorBoundary/ErrorBoundary'
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 ReactDOM.render(
   <>
-{table(5,15)}
+<App/>
   </>,
   document.getElementById("root")
 );
